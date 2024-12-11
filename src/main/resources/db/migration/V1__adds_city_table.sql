aa45bd411653ab6a6ac79bb2ed714c403d2f03e4
Harris, Joshua W SSG USARMY (USA) <joshua.harris.mil@swf.army.mil>
2024-12-10 21:47:17 -0800

    updated DB structure

---
+++ src/main/resources/db/migration/V1__adds_city_table.sql
@@ -0,0 +1,22 @@
+create table IF NOT EXISTS city
+(
+    id integer,
+    city_name varchar(50),
+    population integer
+    );
+INSERT INTO public.city (id, city_name, population)
+VALUES (1, 'Coral Reef Community',150000);
+INSERT INTO public.city (id, city_name, population)
+VALUES (2, 'Turtleback Island', 30000);
+INSERT INTO public.city (id, city_name, population)
+VALUES (3, 'Sunset Cove District', 225000);
+INSERT INTO public.city (id, city_name, population)
+VALUES (4, 'Mangrove Coastline', 350000);
+INSERT INTO public.city (id, city_name, population)
+VALUES (5, 'Lagoon Bay Village', 130000);
+INSERT INTO public.city (id, city_name, population)
+VALUES (6, 'TF Main', 0);
+INSERT INTO public.city (id, city_name, population)
+VALUES (7, 'Stormbreaker Cliffs', 50000);
+INSERT INTO public.city (id, city_name, population)
+VALUES (8, 'Moonlit Cove', 80000 );
