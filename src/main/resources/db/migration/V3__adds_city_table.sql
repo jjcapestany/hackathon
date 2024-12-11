39f70f72f996bc2564096215e2912e2d124914aa
Fishback, Christopher J 1LT USARMY (USA) <christopher.fishback.mil@swf.army.mil>
2024-12-10 19:22:59 -0600

    fixed city table

---
+++ src/main/resources/db/migration/V3__adds_city_table.sql
@@ -0,0 +1,13 @@
+create table IF NOT EXISTS city
+(
+    id   integer,
+    city_name varchar
+);
+INSERT INTO public.city (id, city_name) VALUES (1, 'Coral Reef Community');
+INSERT INTO public.city (id, city_name) VALUES (2, 'Turtleback Island');
+INSERT INTO public.city (id, city_name) VALUES (3, 'Sunset Cove District');
+INSERT INTO public.city (id, city_name) VALUES (4, 'Mangrove Coastline');
+INSERT INTO public.city (id, city_name) VALUES (5, 'Lagoon Bay Village');
+INSERT INTO public.city (id, city_name) VALUES (6, 'Seabreeze Settlement');
+INSERT INTO public.city (id, city_name) VALUES (7, 'Stormbreaker Cliffs');
+INSERT INTO public.city (id, city_name) VALUES (8, 'Moonlit Key');
