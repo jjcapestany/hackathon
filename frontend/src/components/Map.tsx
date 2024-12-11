ab91d33c4771c5d8e60def693a033357d086fbc5
Harris, Joshua W SSG USARMY (USA) <joshua.harris.mil@swf.army.mil>
2024-12-10 23:08:22 -0800

    bg fit as image of map

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,53 +1,14 @@
-import {ImageOverlay, MapContainer, Marker, Popup} from 'react-leaflet';
-import L from 'leaflet';
+import {Stack} from "@mui/material";
+
 
 const Map = () => {
-    // Define bounds (corners of the image in LatLng)
-    const controlBounds =
-        new L.LatLngBounds(
-            new L.LatLng(0, 0),
-            new L.LatLng(1000, 1800)
-        );
-
-    const mapBounds =
-        new L.LatLngBounds(
-            new L.LatLng(0, 0),
-            new L.LatLng(1400, 2200)
-        );
-
-    // URL to your static image
-    const imageUrl = '/island_map.png';
 
     return (
-        <MapContainer
-            scrollWheelZoom={false}
-            doubleClickZoom={false}
-            maxBoundsViscosity={1.0}
-            maxBounds={mapBounds}
-            crs={L.CRS.Simple} // Use a simple CRS for static image
-            bounds={controlBounds}
-            style={{
-                width: '100vw',
-                height: '100vh',
-                position: 'relative',
-            }}
-        >
-            <ImageOverlay url={imageUrl} bounds={mapBounds}/>
-            <Marker
-                position={[1900, 280]}
-                icon={L.divIcon({
-                    html: `<div style="color: black; font-weight: bold; font-size: 70px;">Turtleback Island</div>`,
-                    iconSize: [540, 150],
-                })}
-            >
-                <Popup>
-                    <div>
-                        <h3>You have selected death</h3>
-                        <p>No supplies remaining.</p>
-                    </div>
-                </Popup>
-            </Marker>
-        </MapContainer>
+        <Stack height="100vh" width={"100vw"} sx={{
+            backgroundImage: `url(./island_map.png)`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center'
+        }}>hello</Stack>
     );
 };
 
