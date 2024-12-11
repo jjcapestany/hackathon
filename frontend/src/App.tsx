7ed0570590fd61676d06c0d9f1b5fe94b3df1f18
Johnathan Riemerspeters <johnriemers@gmail.com>
2024-12-11 08:34:23 -0600

    lol we suck

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import Konva from "konva";
 import {Circle, Layer, Rect, Stage} from "react-konva";
 
 //backgroundColor: "#c5ecff"
@@ -8,8 +7,8 @@ function App() {
     return (
         <Stage width={window.innerWidth} height={window.innerHeight}>
             <Layer>
-                <Rect width={50} height={50} fill="red" />
-                <Circle x={200} y={200} stroke="black" radius={50} />
+                <Rect width={50} height={50} fill="red"/>
+                <Circle x={200} y={200} stroke="black" radius={50}/>
             </Layer>
         </Stage>
     )
