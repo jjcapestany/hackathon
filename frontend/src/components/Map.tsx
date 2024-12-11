2ca500c08d729b7de8b57b9c2c543bb8132d9ff4
Fishback, Christopher J 1LT USARMY (USA) <christopher.fishback.mil@swf.army.mil>
2024-12-11 08:28:28 -0600

    a mess

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,54 +1,17 @@
-import {ImageOverlay, MapContainer, Marker, Popup} from 'react-leaflet';
-import L from 'leaflet';
+import { Stage, Layer, Rect, Circle } from 'react-konva';
 
 const Map = () => {
-    // Define bounds (corners of the image in LatLng)
-    const controlBounds =
-        new L.LatLngBounds(
-            new L.LatLng(0, 0),
-            new L.LatLng(1000, 1800)
-        );
-
-    const mapBounds =
-        new L.LatLngBounds(
-            new L.LatLng(0, 0),
-            new L.LatLng(1400, 2200)
-        );
-
-    // URL to your static image
-    const imageUrl = '/island_map.png';
-
     return (
-        <MapContainer
-            scrollWheelZoom={false}
-            doubleClickZoom={false}
-            maxBoundsViscosity={1.0}
-            maxBounds={mapBounds}
-            crs={L.CRS.Simple} // Use a simple CRS for static image
-            bounds={controlBounds}
-            style={{
-                width: '100vw',
-                height: '100vh',
-                position: 'relative',
-            }}
-        >
-            <ImageOverlay url={imageUrl} bounds={mapBounds}/>
-            <Marker
-                position={[1900, 280]}
-                icon={L.divIcon({
-                    html: `<div style="color: black; font-weight: bold; font-size: 70px;">Turtleback Island</div>`,
-                    iconSize: [540, 150],
-                })}
-            >
-                <Popup>
-                    <div>
-                        <h3>You have selected death</h3>
-                        <p>No supplies remaining.</p>
-                    </div>
-                </Popup>
-            </Marker>
-        </MapContainer>
+        // Stage - is a div wrapper
+        // Layer - is an actual 2d canvas element, so you can have several layers inside the stage
+        // Rect and Circle are not DOM elements. They are 2d shapes on canvas
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+            <Layer>
+                <Rect width={50} height={50} fill="red" />
+                <Circle x={200} y={200} stroke="black" radius={50} />
+            </Layer>
+        </Stage>
     );
-};
+}
 
 export default Map;
