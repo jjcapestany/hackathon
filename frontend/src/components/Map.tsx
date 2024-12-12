5f2e64bb0c3b80a18d19ecf7dc3a284b4e2a3ae2
Johnathan Riemerspeters <johnriemers@gmail.com>
2024-12-11 22:46:25 -0600

    makes more responsiver

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -1,15 +1,83 @@
 import {Image, Layer, Stage} from 'react-konva';
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import Cities from "./city/Cities.tsx";
 import {City, getCity} from "./city/CityClient.ts";
 import PopupExamples from "./PopupExamples.tsx";
+import {Box} from "@mui/material";
+import Konva from "konva";
+import {Vector2d} from "konva/lib/types";
+import {Node} from "konva/lib/Node";
+
+
+export const MIN_ZOOM = 0.8;
+export const MAX_ZOOM = 1.3;
+export const IMAGE_WIDTH = 2100
+export const IMAGE_HEIGHT = 1280
+
+function clamp(val, min, max) {
+    return val > max ? max : val < min ? min : val
+}
 
 const Map = () => {
-    const stageWidth = window.innerWidth;
-    const stageHeight = window.innerHeight;
+    const [stageSize, setStageSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
     const [backgroundImage, setBackgroundImage] = useState<CanvasImageSource | undefined>(undefined);
     const [city, setCity] = useState<City[]>([{} as City])
     const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [_, setScale] = useState<number>(1);
+    const stageRef = useRef<Konva.Stage | null>(null);
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const imageRef = useRef<Konva.Image | null>(null);
+
+    const updateStageSize = () => {
+        if (containerRef.current) {
+            setStageSize({
+                width: containerRef.current.offsetWidth,
+                height: containerRef.current.offsetHeight,
+            });
+        }
+    };
+
+    const handleWheel = (e: Konva.KonvaEventObject<WheelEvent>) => {
+        e.evt.preventDefault();
+        if (stageRef.current) {
+            const stage = stageRef.current;
+            const scaleBy = 1.05; // Zoom factor
+            const oldScale = stage.scaleX();
+            const pointer = stage.getPointerPosition();
+            if (!pointer) return;
+            const mousePointTo = {
+                x: (pointer.x - stage.x()) / oldScale,
+                y: (pointer.y - stage.y()) / oldScale,
+            };
+            const newScale = e.evt.deltaY > 0 ? oldScale / scaleBy : oldScale * scaleBy;
+            if (newScale < MIN_ZOOM || newScale > MAX_ZOOM) return;
+            setScale(newScale);
+            const newPos = {
+                x: pointer.x - mousePointTo.x * newScale,
+                y: pointer.y - mousePointTo.y * newScale,
+            };
+
+            stage.scale({x: newScale, y: newScale});
+            stage.position(newPos);
+            console.log(stage.position())
+            stage.batchDraw(); // Redraw
+        }
+    };
+
+    const handleDrag = (pos: Vector2d) => {
+        console.log(imageRef.current?.x())
+        console.log(imageRef.current?.y())
+        console.log(pos)
+        return {
+            x: clamp(pos.x, stageRef.current && (-IMAGE_WIDTH * stageRef.current?.scaleX() * 2) / 4,
+                stageRef.current && (IMAGE_WIDTH * stageRef.current?.scaleX() * 2) / 4),
+            y: clamp(pos.y, stageRef.current && (-IMAGE_HEIGHT * stageRef.current?.scaleY() * 2) / 4,
+                stageRef.current && (IMAGE_HEIGHT * stageRef.current?.scaleY() * 2) / 4),
+        }
+    }
 
     useEffect(() => {
         setIsLoading(true)
@@ -23,28 +91,46 @@ const Map = () => {
         img.onload = () => {
             setBackgroundImage(img);
         }
-    }, []);
-    console.log(city)
-    return (
-        !isLoading ?
 
-            < Stage
-                width={stageWidth}
-                height={stageHeight}>
-                < Layer>
-                    < Image
-                        image={backgroundImage}
-                        width={stageWidth}
-                        height={stageHeight}
-                        listening={false} // Ensures the background doesn't interfere with other interactions
-                    />
-                </Layer>
-                <Cities cities={city}/>
-                {/*<Layer><IconExamples/></Layer>*/}
-                <Layer><PopupExamples/></Layer>
-            </Stage> :
-            <></>
-    )
+        // Update the size of the canvas on window resize
+        window.addEventListener('resize', updateStageSize);
+        window.addEventListener('mousedown', updateStageSize);
+        updateStageSize(); // Initial call
+        return () => {
+            window.removeEventListener('resize', updateStageSize);
+        };
+    }, []);
+
+
+    return (
+        <Box ref={containerRef} height={"100%"} width={"100%"}>
+            {!isLoading ?
+                < Stage
+                    draggable
+                    dragBoundFunc={handleDrag}
+                    onWheel={handleWheel}
+                    ref={stageRef}
+                    width={stageSize.width}
+                    height={stageSize.height}
+                >
+                    < Layer>
+                        < Image
+                            ref={imageRef}
+                            image={backgroundImage}
+                            x={(stageSize.width - IMAGE_WIDTH) / 2}
+                            y={(stageSize.height - IMAGE_HEIGHT) / 2}
+                            width={IMAGE_WIDTH}
+                            height={IMAGE_HEIGHT}
+                            listening={false} // Ensures the background doesn't interfere with other interactions
+                        />
+                    </Layer>
+                    <Cities cities={city} imageRef={imageRef}/>
+
+                    {/*<Layer><IconExamples/></Layer>*/}
+                    <Layer><PopupExamples/></Layer>
+                </Stage> :
+                <></>}
+        </Box>)
 }
 
 export default Map;
--- frontend/src/components/city/Cities.tsx
+++ frontend/src/components/city/Cities.tsx
@@ -1,15 +1,17 @@
 import {Circle, Group, Image, Layer, Text} from "react-konva";
 import {City} from "./CityClient.ts";
-import CityStatusModal from "./CityStatusModal.tsx";
-import {useEffect, useState} from "react";
-import {stageHeight, stageWidth} from "../../types.ts";
+import {MutableRefObject, useEffect, useState} from "react";
 import city_black from "../../assets/city_black.png";
+import Konva from "konva";
+import {IMAGE_HEIGHT, IMAGE_WIDTH} from "../Map.tsx";
+import CityStatusModal from "./CityStatusModal.tsx";
 
 type CitiesProps = {
-    cities: City[]
+    cities: City[],
+    imageRef: MutableRefObject<Konva.Image | null>
 }
 
-const Cities = ({cities}: CitiesProps) => {
+const Cities = ({cities, imageRef}: CitiesProps) => {
     const [isCityStatusOpen, setIsCityStatusOpen] = useState(false);
     const [cityStatusContent, setCityStatusContent] = useState<City>({} as City)
     const [cityImage, setCityImage] = useState<CanvasImageSource | undefined>(undefined);
@@ -41,10 +43,12 @@ const Cities = ({cities}: CitiesProps) => {
             )}
             <Layer>
                 {cities.map((c, index) => (
-                    <Group key={index} x={stageWidth * c.xcoord} y={stageHeight * c.ycoord} draggable onClick={() => handleCityStatusClick(c.name)}>
-                        <Text text={c.name} />
-                        <Circle x={50} y={40} radius={25} fill={"#ffff66"} />
-                        <Image x={35} y={25} width={30} height={30} image={cityImage} />
+                    <Group key={index} x={(imageRef.current?.x() ?? 0) + IMAGE_WIDTH * c.xcoord}
+                           y={(imageRef.current?.y() ?? 0) + IMAGE_HEIGHT * c.ycoord} draggable
+                           onClick={() => handleCityStatusClick(c.name)}>
+                        <Text text={c.name}/>
+                        <Circle x={50} y={40} radius={25} fill={"#ffff66"}/>
+                        <Image x={35} y={25} width={30} height={30} image={cityImage}/>
                     </Group>
                     // <Circle
                     //     key={index}
