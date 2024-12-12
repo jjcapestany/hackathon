0f419437b29a3743908446020ba699c6a3a4eb10
Fishback, Christopher J 1LT USARMY (USA) <christopher.fishback.mil@swf.army.mil>
2024-12-11 23:26:11 -0600

    added tick for fuel consumption on backend

--- src/main/java/com/hacks/hackathon/city/CityService.java
+++ src/main/java/com/hacks/hackathon/city/CityService.java
@@ -1,8 +1,11 @@
 package com.hacks.hackathon.city;
 
+import com.hacks.hackathon.asset.Asset;
 import com.hacks.hackathon.resources.ResourceType;
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +20,41 @@ public class CityService {
 
     public City getCityById(Long id) {
         return cityRepo.findById(id).orElse(null);
+    }
+
+    public final int TICK_RATE_MS = 10000;
+
+    @Scheduled(fixedDelay = TICK_RATE_MS)
+    @Transactional
+    public void consumeAid() {
+        System.out.println("AID HAS BEEN CONSUMED");
+        List<City> cities = getAllCities();
+
+        for (City city : cities) {
+            consumeResource(city, ResourceType.WATER, city.getWater().getUsageRate());
+            consumeResource(city, ResourceType.FOOD, city.getFood().getUsageRate());
+            consumeResource(city, ResourceType.MEDICAL, city.getMedical().getUsageRate());
+        }
+    }
+
+    private void consumeResource(City city, ResourceType resourceType, double usageRate) {
+        Long currentOnHand = getCurrentResourceOnHand(city, resourceType);
+        Long amountConsumed = Math.negateExact( (long) usageRate);
+
+        modifyCityResources(city.getId(), resourceType, amountConsumed);
+
+        if (currentOnHand-amountConsumed <= 0 && !city.getName().equals("TF Main")) {
+            System.out.printf("Warning: %s in city %s has been depleted.%n", resourceType, city.getName());
+        }
+    }
+
+    private Long getCurrentResourceOnHand(City city, ResourceType resourceType) {
+        return switch (resourceType) {
+            case WATER -> city.getWater().getOnHand();
+            case FOOD -> city.getFood().getOnHand();
+            case MEDICAL -> city.getMedical().getOnHand();
+            default -> throw new IllegalArgumentException("Unknown resource type: " + resourceType);
+        };
     }
 
     public boolean modifyCityResources(Long cityId, ResourceType resource, Long resourceQuantity) {
